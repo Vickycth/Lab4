c104b7f11349dd75442a496bffbe539434bba758
Vicky <tianhui2@illinois.edu>
2018-09-26 18:39:12 -0500

    ??

--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -70,6 +70,7 @@ public class Pokemon {
         this.attackLevel = 0;
         this.defenseLevel = 0;
         this.name = "";
+
     }
 
     /**
@@ -94,6 +95,7 @@ public class Pokemon {
         int attackBonus = d20.roll();
         int defenseBonus = d20.roll();
 
+
         /*
          * Roll the damage dice and compute total damage.
          */
@@ -101,6 +103,7 @@ public class Pokemon {
         int damage2 = d6.roll();
         int damage3 = d6.roll();
         int totalDamage = damage1 + damage2 + damage3;
+
 
         System.out.println(this.name + " is attacking " + opponent.name);
         System.out.println(this.name + " rolls an attack bonus of " + attackBonus);
@@ -111,13 +114,6 @@ public class Pokemon {
          * Did our attack hit?
          */
         if ((attackLevel + attackBonus) > (opponent.defenseLevel + defenseBonus)) {
-            System.out.println("The attack hits dealing 3-D6 damage!");
-            System.out.println("The rolls are " + damage1 + ", " + damage2 + ", " + "and "
-                    + damage3 + " totaling: " + totalDamage + " damage!");
-
-            /*
-             * Does opponent have hit points left?
-             */
             if ((opponent.hitPoints - totalDamage) > 0) {
                 System.out.println(opponent.name + " has "
                         + (opponent.hitPoints - totalDamage) + " hit points");
@@ -130,8 +126,11 @@ public class Pokemon {
             opponent.hitPoints = opponent.hitPoints - totalDamage;
         } else {
             System.out.println("The attack missed!");
+            System.out.print("fail");
+
         }
         System.out.println(" ");
         return (opponent.hitPoints < 1);
     }
+
 }
